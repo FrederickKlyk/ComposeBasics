7bad8a8b938337d37a1d552ade6f52eb2b42d3f1
Frederick Klyk <klyk@adesso-mobile.de>
2021-11-16 15:22:42 +0100

    main: add secondViewPreview mit callbacks

--- feature/secondfeature/src/main/java/de/fred/composedemo1/secondfeature/ui/SecondFeatureView.kt
+++ feature/secondfeature/src/main/java/de/fred/composedemo1/secondfeature/ui/SecondFeatureView.kt
@@ -5,32 +5,65 @@ import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.SavedStateHandle
 import org.koin.androidx.compose.viewModel
 import org.koin.core.parameter.parametersOf
 
 @Composable
 fun SecondFeatureView() {
-    val viewModel: SecondFeatureViewModel by viewModel{ parametersOf(SavedStateHandle())}
-    val uiStateFlow = viewModel.uiStateFlow.collectAsState()
+    val viewModel: SecondFeatureViewModel by viewModel { parametersOf(SavedStateHandle()) }
+    val uiStateFlow by viewModel.uiStateFlow.collectAsState()
     val uiState = viewModel.uiState
 
+    SecondFeatureContent(
+        uiStateFlow = uiStateFlow,
+        uiState = uiState,
+        incrementUiStateInteger = viewModel::incrementUiStateInteger,
+        downloadFakeData = viewModel::downloadFakeData,
+        navigateToThirdFeatureModule = viewModel::navigateToThirdFeatureModule
+    )
+}
+
+@Composable
+fun SecondFeatureContent(
+    uiStateFlow: Int,
+    uiState: SecondFeatureUIState,
+    incrementUiStateInteger: () -> Unit,
+    downloadFakeData: () -> Unit,
+    navigateToThirdFeatureModule: () -> Unit,
+) {
     Column() {
-        when(uiState){
-            is SecondFeatureUIState.error -> { Text("Fehler wegen: ${uiState.message}")}
-            SecondFeatureUIState.initial -> { Text("Initialier Zustand")}
-            SecondFeatureUIState.loaded -> { Text("Fertig geladen")}
-            is SecondFeatureUIState.loading -> { Text("Progress: ${uiState.progress}")}
+        when (uiState) {
+            is SecondFeatureUIState.error -> {
+                Text("Fehler wegen: ${uiState.message}")
+            }
+            SecondFeatureUIState.initial -> {
+                Text("Initialier Zustand")
+            }
+            SecondFeatureUIState.loaded -> {
+                Text("Fertig geladen")
+            }
+            is SecondFeatureUIState.loading -> {
+                Text("Progress: ${uiState.progress}")
+            }
         }
-        Text("Hallo, dies ist das secondFeature Module, die Intnumber von stateFlow: ${uiStateFlow.value}")
-        Button(onClick = { viewModel.incrementUiStateInteger() }) {
+        Text("Hallo, dies ist das secondFeature Module, die Intnumber von stateFlow: ${uiStateFlow}")
+        Button(onClick = incrementUiStateInteger) {
             Text("Erhöhe die Zahl")
         }
-        Button(onClick = { viewModel.downloadFakeData() }) {
+        Button(onClick = downloadFakeData) {
             Text("Lade etwas runter")
         }
-        Button(onClick = viewModel::navigateToThirdFeatureModule) {
+        Button(onClick = navigateToThirdFeatureModule) {
             Text("weiter zum dritten FeatureModule")
         }
     }
+}
+
+@Preview
+@Composable
+private fun SecondFeatureContentPreview() {
+    SecondFeatureContent(1, SecondFeatureUIState.initial, {}, {}, {})
 }
