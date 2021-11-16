b362ad9bf6d643aeff27e45c606d2a816761677e
Frederick Klyk <klyk@adesso-mobile.de>
2021-11-16 15:26:40 +0100

    main: add preview to ThirdFeature

--- feature/thirdfeature/src/main/java/de/fred/composedemo1/thirdfeature/ui/ThridFeatureView.kt
+++ feature/thirdfeature/src/main/java/de/fred/composedemo1/thirdfeature/ui/ThridFeatureView.kt
@@ -4,16 +4,28 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import org.koin.androidx.compose.get
 
 @Composable
 fun ThirdFeatureView() {
+    val viewModel: ThirdFeatureViewModel = get()
+    ThirdFeatureContent(viewModel::navigateToSecondFeatureModule)
+}
+
+@Composable
+fun ThirdFeatureContent(navigateToSecondFeatureModule: () -> Unit) {
     Column() {
-        val viewModel: ThirdFeatureViewModel = get()
         Text("Hallo, dies ist das ThirdFeature Module")
         Text("Das dritte Modul hat keine Abhängikeit zum zweiten Modul, navigation somit über Navigation Middlelayer möglich..")
-        Button(onClick = viewModel::navigateToSecondFeatureModule) {
+        Button(onClick = navigateToSecondFeatureModule) {
             Text("zurück zum zweiten Modul")
         }
     }
+}
+
+@Preview
+@Composable
+fun ThirdFeatureContentPreview() {
+    ThirdFeatureContent {}
 }
