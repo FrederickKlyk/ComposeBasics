573a19a77842d45532244e189d2cda47143e95e2
Fred <frederickklyk@gmail.com>
2021-11-17 21:16:56 +0100

    drop oldest on bufferoverflow sharedFlow navigation

--- feature/thirdfeature/src/main/java/de/fred/composedemo1/thirdfeature/ui/ThirdFeatureViewModel.kt
+++ feature/thirdfeature/src/main/java/de/fred/composedemo1/thirdfeature/ui/ThirdFeatureViewModel.kt
@@ -1,11 +1,22 @@
 package de.fred.composedemo1.thirdfeature.ui
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import de.fred.composedemo1.navigation.Navigator
 
 class ThirdFeatureViewModel(private val navigator: Navigator) : ViewModel(){
 
+    init {
+        Log.d("ThirdFeatureViewModel", "ThirdFeatureViewModel wurde initialisiert...")
+
+    }
+
     fun navigateToSecondFeatureModule(){
         navigator.navigateTo(Navigator.NavTarget.SecondModule)
+    }
+
+    override fun onCleared() {
+        Log.d("ThirdFeatureViewModel", "ThirdFeatureViewModel wurde abgeräumt...")
+        super.onCleared()
     }
 }
--- navigation/src/main/java/de/fred/composedemo1/navigation/Navigator.kt
+++ navigation/src/main/java/de/fred/composedemo1/navigation/Navigator.kt
@@ -1,11 +1,12 @@
 package de.fred.composedemo1.navigation
 
+import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 
 class Navigator {
 
-    private val _sharedFlow = MutableSharedFlow<NavTarget>(extraBufferCapacity = 1)
+    private val _sharedFlow = MutableSharedFlow<NavTarget>(extraBufferCapacity = 1, onBufferOverflow = BufferOverflow.DROP_OLDEST)
     val sharedFlow = _sharedFlow.asSharedFlow()
 
     fun navigateTo(navTarget: NavTarget) {
