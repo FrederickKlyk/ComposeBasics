e493f2fcde4a4f48e38fc995702d187638435714
Frederick Klyk <klyk@adesso-mobile.de>
2021-11-23 11:49:59 +0100

    main: remove debug code

--- navigation/src/main/java/de/fred/composedemo1/navigation/NavTarget.kt
+++ navigation/src/main/java/de/fred/composedemo1/navigation/NavTarget.kt
@@ -2,6 +2,7 @@ package de.fred.composedemo1.navigation
 
 sealed class NavTarget(val label: String) {
     object RootModule : NavTarget(ModuleRoutes.RootModule.label)
+
     sealed class SecondFeatureModule {
         object SecondFeature : NavTarget("${ModuleRoutes.SecondFeature.label}/{secondId}")
         data class SecondModuleWithParams(val secondId: String) : NavTarget("secondmodule/$secondId")
@@ -12,10 +13,6 @@ sealed class NavTarget(val label: String) {
     object Home : NavTarget(ModuleRoutes.Home.label)
     object Detail : NavTarget(ModuleRoutes.Detail.label)
     object ThirdFeature : NavTarget(ModuleRoutes.ThirdFeature.label)
-
-    sealed class Test {
-        object TestClass : NavTarget("secondmodule/tersrwesa")
-    }
 }
 
 // Screen Targets
