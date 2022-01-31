ba4b4d0674cb23595a5f5a29d146b71d23bfda36
Fred <frederickklyk@gmail.com>
2022-01-31 19:29:56 +0100

    revert changes in second Feature Module

--- feature/secondfeature/src/main/java/de/fred/composedemo1/secondfeature/ui/SecondFeatureView.kt
+++ feature/secondfeature/src/main/java/de/fred/composedemo1/secondfeature/ui/SecondFeatureView.kt
@@ -1,54 +1,92 @@
 package de.fred.composedemo1.secondfeature.ui
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.tooling.preview.Preview
+import de.fred.composedemo1.navigation.DrawerRoute
+import de.fred.composedemo1.navigation.NavTarget
+import de.fred.designsystem.buttons.DefaultDrawer
+import de.fred.designsystem.buttons.DefaultTopBar
 
 @Composable
-fun FeatureSecondContent(viewModel: FeatureSecondViewModel) {
+fun FeatureSecondContent(viewModel: FeatureSecondViewModel, secondFeatureModuleID: String) {
     val uiStateFlow by viewModel.uiStateFlow.collectAsState()
     val uiState = viewModel.uiState
 
     FeatureSecondContent(
+        secondFeatureModuleID = secondFeatureModuleID,
         uiStateFlow = uiStateFlow,
         uiState = uiState,
-        incrementUiStateFlowInteger = viewModel::incrementUiStateFlowInteger,
-        downloadDataFromRepository = viewModel::downloadDataFromRepository,
+        incrementUiStateInteger = viewModel::incrementUiStateFlowInteger,
+        downloadFakeData = viewModel::downloadDataFromRepository,
+        navigateToThirdFeatureModule = viewModel::navigateToThirdFeatureModule
     )
 }
 
+
 @Composable
 fun FeatureSecondContent(
+    secondFeatureModuleID: String,
     uiStateFlow: Int,
     uiState: FeatureSecondUIState,
-    incrementUiStateFlowInteger: () -> Unit,
-    downloadDataFromRepository: () -> Unit,
+    incrementUiStateInteger: () -> Unit,
+    downloadFakeData: () -> Unit,
+    navigateToThirdFeatureModule: () -> Unit,
 ) {
-    Column() {
-        when (uiState) {
-            is FeatureSecondUIState.Error -> {
-                Text("Fehler wegen: ${uiState.message}.")
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+
+    ModalDrawer(
+        drawerState = drawerState,
+        gesturesEnabled = drawerState.isOpen,
+        drawerContent = {
+            DefaultDrawer(
+                scope = scope,
+                drawerState = drawerState,
+                screens = listOf(DrawerRoute("Main", NavTarget.RootModule)),
+                onDestinationClicked = { route ->
+
+                }
+            )
+        },
+    ) {
+        Column() {
+            DefaultTopBar(
+                scope = scope,
+                drawerState = drawerState,
+                title = "Second Feature",
+                buttonIcon = Icons.Filled.Menu,
+            )
+            when (uiState) {
+                is FeatureSecondUIState.Error -> {
+                    Text("Fehler wegen: ${uiState.message}")
+                }
+                FeatureSecondUIState.Initial -> {
+                    Text("Initialier Zustand")
+                }
+                FeatureSecondUIState.Loaded -> {
+                    Text("Fertig geladen")
+                }
+                is FeatureSecondUIState.Loading -> {
+                    Text("Progress: ${uiState.progress}")
+                }
             }
-            FeatureSecondUIState.Initial -> {
-                Text("Initialier UI-Zustand mit einen von einem StateFlow zu " +
-                        "einem State gecasteten Zustand: $uiStateFlow")
+            Text("Hallo, dies ist das secondFeature Module mit dem Übergabeparamter $secondFeatureModuleID, die Intnumber von stateFlow: $uiStateFlow")
+            Button(onClick = incrementUiStateInteger) {
+                Text("Erhöhe die Zahl")
             }
-            FeatureSecondUIState.Loaded -> {
-                Text("Erfolgreich geladen.")
+            Button(onClick = downloadFakeData, enabled = uiState !is FeatureSecondUIState.Loading) {
+                Text("Lade etwas runter")
             }
-            is FeatureSecondUIState.Loading -> {
-                Text("Progress: ${uiState.progress}")
+            Button(onClick = navigateToThirdFeatureModule) {
+                Text("weiter zum dritten FeatureModule")
             }
-        }
-        Button(onClick = incrementUiStateFlowInteger) {
-            Text("Erhöhe die Zahl")
-        }
-        Button(onClick = downloadDataFromRepository, enabled = uiState !is FeatureSecondUIState.Loading) {
-            Text("Lade etwas runter")
         }
     }
 }
@@ -56,5 +94,5 @@ fun FeatureSecondContent(
 @Preview
 @Composable
 private fun FeatureSecondContentPreview() {
-    FeatureSecondContent(1, FeatureSecondUIState.Initial, {}, {})
+    FeatureSecondContent("", 1, FeatureSecondUIState.Initial, {}, {}, {})
 }
